41927642adf7e55434ac2bbe7dcc23b479e497d3
NirBD <62333490+NirbDor@users.noreply.github.com>
2021-11-15 21:34:50 +0200

    Update

--- pcenterApp/tests.py
+++ pcenterApp/tests.py
@@ -6,6 +6,14 @@ class URLTests(TestCase):
         response = self.client.post("/s/create", {'full_link':'https://ravkavonline.co.il'})
         self.assertEqual(response.status_code, 200)
 
+    def test_unique_shortcuts(self):
+        shortcut_response1 = self.client.post("/s/create", {'full_link':'https://ravkavonline.co.il'})
+        shortcut_response2 = self.client.post("/s/create", {'full_link':'https://ravkavonline.co.il'})
+        
+        shortcut1 = Url.objects.get(id=1).shortcut
+        shortcut2 = Url.objects.get(id=2).shortcut
+
+        self.assertNotEqual(shortcut1,shortcut2)
 
     def test_redirect_page(self):
         shortcut_response = self.client.post("/s/create", {'full_link':'https://ravkavonline.co.il'})
@@ -17,4 +25,5 @@ class URLTests(TestCase):
         none_existing_url = "123\><+&^%@"
         response = self.client.get("/s/"+none_existing_url)
         self.assertEqual(response.status_code, 404)
+        
         
